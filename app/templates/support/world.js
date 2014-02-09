e33c74a705336034bde413ea91869fed8de5f5c7
Sartaj <sartaj@sartaj.me>
2014-02-08 18:23:17 -0600

    Remove .browser from Zombie.
    Zombie doesn't need .browser to instantiate anymore.

--- app/templates/support/world.js
+++ app/templates/support/world.js
@@ -1,6 +1,6 @@
 var zombie = require('zombie');
 var World = function World(callback) {
-    this.browser = new zombie.Browser(); // this.browser will be available in step definitions
+    this.browser = new zombie // this.browser will be available in step definitions
 
     this.visit = function (url, callback) {
         this.browser.visit(url, callback);
